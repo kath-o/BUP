2f1c52876eb26a09f5b344444d6823c68634bb20
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-01 14:27:43 +0100

    loading and subsetting data

--- BuPW3.R
+++ BuPW3.R
@@ -5,3 +5,17 @@
 install.packages("raster",dependencies=TRUE,repos="https://cloud.r-project.org")
 library(raster)
 
+#loading data
+avi_dat <- read.table('Data_SwissBreedingBirds.csv', header=T, sep=',')
+
+nrow(avi_dat)
+
+summary(avi_dat)
+
+#subset the data to select columns were working with 
+
+ouzel_cols <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
+
+ouzel_df <- data.frame(avi_dat)[ouzel_cols]
+
+summary(ouzel_df)
