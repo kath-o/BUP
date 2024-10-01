249792ec5fe8e27ee57f5420e20f79ed57db510f
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-01 18:52:18 +0100

    change to geodata and terra

--- BuPW3.R
+++ BuPW3.R
@@ -1,21 +1,17 @@
 #new project 
 #sorting git 
+#fixed error using "git config --global http.postBuffer 524288000" - ask
 
 #installing raster
 install.packages("raster",dependencies=TRUE,repos="https://cloud.r-project.org")
 library(raster)
 
-#loading data
-avi_dat <- read.table('Data_SwissBreedingBirds.csv', header=T, sep=',')
+#change to geodata and terra
 
-nrow(avi_dat)
+install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org")
+library(geodata)
+library(terra)
 
-summary(avi_dat)
 
-#subset the data to select columns were working with 
-
-ouzel_cols <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
-
-ouzel_df <- data.frame(avi_dat)[ouzel_cols]
 
 summary(ouzel_df)
