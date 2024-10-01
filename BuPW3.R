cac7ddc460c0ea97f8610e075a25659a14369db3
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-01 19:00:26 +0100

    reproject the raster data in Swiss coordinates and first crop the layers for the Swiss boundary

--- BuPW3.R
+++ BuPW3.R
@@ -35,3 +35,24 @@ output_dir<-"~/Desktop/MSc EEB/WD/BUP/Data"
 bio_curr <-worldclim_country("Switzerland",version="2.1", var='bio', res=10, lon=5.5, lat=45.5, path=output_dir)[[c(2,5,14)]]
 
 bio_fut <- cmip6_world(var = "bio", model = "CNRM-CM6-1-HR", ssp = "245", res = 10,  time = "2041-2060",  lon = c(5.96, 10.49),  lat = c(45.82, 47.81),path=output_dir)[[c(2,5,14)]]
+
+#next step is to reproject the raster data in Swiss coordinates and first crop the layers for the Swiss boundary
+
+# A spatial mask of Switzerland in Swiss coordinates
+bg <- rast('/vsicurl/https://damariszurell.github.io/SDM-Intro/CH_mask.tif')
+
+bio_curr <- terra::project(bio_curr, bg)
+bio_fut <- terra::project(bio_fut, bg)
+#we need to change the projection of our cliamte data to match that of the bg file.
+
+bio_curr <- terra::resample(bio_curr, bg)
+bio_fut <- terra::resample(bio_fut, bg)
+#we then need to make the resolution equivalent to bg. 
+
+
+bio_curr <- terra::mask(bio_curr, bg)
+bio_fut <- terra::mask(bio_fut, bg)
+#we then need to clip the extent to match an outline of Switzerland
+
+names(bio_curr) <- c('bio_2', 'bio_5', 'bio_14')
+names(bio_fut) <- c('bio_2', 'bio_5', 'bio_14')
