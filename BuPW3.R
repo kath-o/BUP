2e9ee7765063108f1604d9c9f371419b02a94295
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-01 18:53:28 +0100

    loading, summarising and subsetting data

--- BuPW3.R
+++ BuPW3.R
@@ -12,6 +12,17 @@ install.packages("geodata",dependencies=TRUE,repos="https://cloud.r-project.org"
 library(geodata)
 library(terra)
 
+#loading data
+avi_dat <- read.table('Data_SwissBreedingBirds.csv', header=T, sep=',')
 
+nrow(avi_dat)
+
+summary(avi_dat)
+
+#subset the data to select columns were working with 
+
+ouzel_cols <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
+
+ouzel_df <- data.frame(avi_dat)[ouzel_cols]
 
 summary(ouzel_df)
