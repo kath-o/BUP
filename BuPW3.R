104b1c27c77689552b4e8bfe746534c7e5787b12
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-01 18:57:46 +0100

    importing current and future climate data

--- BuPW3.R
+++ BuPW3.R
@@ -26,3 +26,12 @@ ouzel_cols <- c('Turdus_torquatus', 'bio_5', 'bio_2', 'bio_14', 'blockCV_tile')
 ouzel_df <- data.frame(avi_dat)[ouzel_cols]
 
 summary(ouzel_df)
+
+#importing current and future climate data for Switzerland and clip it to the extent of Switzerland
+#Download and import same climate variables from WORLDCLIM using geodata package
+
+output_dir<-"~/Desktop/MSc EEB/WD/BUP/Data"
+
+bio_curr <-worldclim_country("Switzerland",version="2.1", var='bio', res=10, lon=5.5, lat=45.5, path=output_dir)[[c(2,5,14)]]
+
+bio_fut <- cmip6_world(var = "bio", model = "CNRM-CM6-1-HR", ssp = "245", res = 10,  time = "2041-2060",  lon = c(5.96, 10.49),  lat = c(45.82, 47.81),path=output_dir)[[c(2,5,14)]]
