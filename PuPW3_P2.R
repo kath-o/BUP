9daaea0083970f8d5bcedc6d1692dfe85e7920e6
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-04 13:48:40 +0100

    change to my species

--- PuPW3_P2.R
+++ PuPW3_P2.R
@@ -9,7 +9,8 @@ library(geodata)
 library(predicts)
 library(terra)
 
-#downloading data from GBIF - first 10000, my species Erythrorchis altissima
-occdata <- geodata::sp_occurrence("Erythrorchis", "altissima*", geo=FALSE,removeZeros=TRUE,start=1,end=10000)
+#downloading data from GBIF - first 10000, my species rimula latifolia
+occdata <- geodata::sp_occurrence("Primula", "latifolia*", geo=FALSE,removeZeros=TRUE,start=1,end=10000)
 
 #star gives us all name variants
+#geo = TRUE only shows 
