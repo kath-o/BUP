487334733d1557747e7627a2cddd9550c9f287bb
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-04 13:53:15 +0100

    plotting my distribution

--- PuPW3_P2.R
+++ PuPW3_P2.R
@@ -13,4 +13,15 @@ library(terra)
 occdata <- geodata::sp_occurrence("Primula", "latifolia*", geo=FALSE,removeZeros=TRUE,start=1,end=10000)
 
 #star gives us all name variants
-#geo = TRUE only shows 
+#geo = TRUE only shows records w lat and long 
+
+#data frame
+dim(occdata)
+occdata[1:10, ]
+
+#plot global distribution to make sure it fits with expectation 
+wrld <- world(path="~/Desktop/MSc EEB/WD/BUP")
+#this function gives us an outline of the world's political boundaries. Reminder, if ever you want to know more about an R function, you can write ?function.name, e.g., ?world
+plot(wrld, xlim=c(-180,180), ylim=c(-80,80), col="light yellow", border="light gray")
+# add the points
+points(occdata$lon, occdata$lat, col='blue', pch=20)
