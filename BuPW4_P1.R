d94f7d3f0b6886b711732f1ff00d497dbaf7f59d
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-07 15:44:55 +0100

    packages

---
+++ BuPW4_P1.R
@@ -0,0 +1,4 @@
+#packages 
+my_packages <- c('dplyr', 'tidyr', 'marked', 'ggplot2', 'R2ucare')
+new_packages <- my_packages[!(my_packages %in% installed.packages()[,'Package'])]
+if(length(new_packages)) install.packages(new_packages)
