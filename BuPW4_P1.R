8acc43885bdb4134e67b7874d6fee491856325de
Kathleen Owen <kathleenowen@MacBook-Air-280.local>
2024-10-08 13:08:16 +0100

    library

--- BuPW4_P1.R
+++ BuPW4_P1.R
@@ -2,3 +2,9 @@
 my_packages <- c('dplyr', 'tidyr', 'marked', 'ggplot2', 'R2ucare')
 new_packages <- my_packages[!(my_packages %in% installed.packages()[,'Package'])]
 if(length(new_packages)) install.packages(new_packages)
+
+library(dplyr)
+library(tidyr)
+library(marked)
+library(ggplot2)
+library(R2ucare)
